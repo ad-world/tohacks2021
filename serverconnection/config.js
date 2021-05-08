092e5d4091c2f1b076d1258dec10b716adad6fe5
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-08 20:33:25 +0400

    mongodb connection

---
+++ serverconnection/config.js
@@ -0,0 +1,6 @@
+// username: tohacks
+// password: tohacks
+
+module.exports = {
+    MONGODB: 'mongodb+srv://tohacks:tohacks@news-aggregator.dzsgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
