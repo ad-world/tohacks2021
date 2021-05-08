f6e8704f52eed02e4b496acfbedd30e8e4e57079
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-09 02:00:40 +0400

    created navbar

--- tohacks/src/App.js
+++ tohacks/src/App.js
@@ -6,19 +6,25 @@ import './App.css';
 
 import Home from './pages/Home'
 import Article from './pages/Article'
+import NavBar from './components/NavBar'
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <Router>
-        <Route exact path='/' component={Home}/>
+    <Container>
+      <NavBar />
+      <Router>
+        <Route exact path='/' component={Home} />
         {/* <Route exact path='/sports' component={Sports}/>
         <Route exact path='/business' component={Business}/>
         <Route exact path='/finance' component={Finance}/>
         <Route exact path='/politics' component={Politics}/> */}
         <Switch>
-            <Route exact path='/:articleId' component={Article} />
+          <Route exact path='/:articleId' component={Article} />
         </Switch>
-    </Router>
+      </Router>
+    </Container>
+
   );
 }
 
