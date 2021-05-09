2d9022a2fee4978d7f0748456ec9ccf69457735b
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-09 10:07:48 +0400

    Revert "updated cards"
    This reverts commit b034aabbb0d89e04d4d70ace376cf6b1fb792b37.

--- tohacks/src/components/NewsCard.js
+++ tohacks/src/components/NewsCard.js
@@ -1,27 +1,31 @@
 import React from 'react'
-import { Card, Icon, Label, Button, Menu } from 'semantic-ui-react'
+import { Card, Icon, Label, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 
 function NewsCard({ article: { id, date, title, link } }) {
 
     return (
-        <Card fluid style={{ height: 250 }}>
+        <Card fluid style={{ height: 200 }}>
             <Card.Content>
                 <Card.Header>
                     {title}
                 </Card.Header>
-                <br></br>
-                <Card.Meta as={Link} to={`/${id}`} onClick={() => {
-                    localStorage.setItem('link', link);
-                }}>
-                    View Article
-                </Card.Meta>
-            </Card.Content>
-            <Card.Content extra>
-                <Label>
-                    {moment(date).fromNow()}
-                </Label>
+                <Card.Content extra>
+                    <Label>
+                        {moment(date).fromNow()}
+                    </Label>
+                    <Icon name='link'>
+                        <Link to={`/${id}`} onClick={() => {
+                            localStorage.setItem('link', link)
+                            console.log(link);
+                        }
+                        }>
+                            Link
+                        </Link>
+                    </Icon>
+                </Card.Content>
+
             </Card.Content>
         </Card>
     );
