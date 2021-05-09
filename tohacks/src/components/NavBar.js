1eefbd6d353416012fd50387eb0ab1164610e748
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-09 12:00:46 +0400

    loading animation

--- tohacks/src/components/NavBar.js
+++ tohacks/src/components/NavBar.js
@@ -1,38 +1,17 @@
 import React, { useState } from 'react'
 import { Input, Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 
 export default function NavBar() {
-    const [current, setCurrent] = useState('latest news');
-    const config = JSON.parse(localStorage.getItem("config"));
-    const handleClick = (e, { name }) => {
-        setCurrent(name)
-        localStorage.setItem('current-tab', name);
-    };
-
     return (
-        <Menu secondary>
-            <Menu.Item
-                name='latest news'
-                active={current === 'latest news'}
-                onClick={handleClick}
-            />
-            {config && Object.entries(config).map((key, val) => {
-                if (key[1]) {
-                    console.log(key)
-                    return (
-                        <Menu.Item
-                            name={key[0]}
-                            active={current === {key}}
-                            onClick={handleClick}
-                        />
-                    )
-                }
-            })}
-            <Menu.Menu position='right'>
-                <Menu.Item>
-                    <Input icon='search' placeholder='Search...' />
-                </Menu.Item>
+        <Menu secondary style={{ marginTop: 9 }}>
+            <Menu.Menu >
+                <Menu.Item
+                    name='Home'
+                    as={Link}
+                    to="/"
+                />
             </Menu.Menu>
         </Menu>
     )
--- tohacks/src/pages/Article.js
+++ tohacks/src/pages/Article.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom'
 import { Container, Grid, Header } from 'semantic-ui-react';
 import moment from 'moment';
 import { useEffect, useState } from 'react'
+import NavBar from '../components/NavBar'
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 function SingleArticle(props) {
 
@@ -15,50 +17,57 @@ function SingleArticle(props) {
     const [authors, setAuthors] = useState("")
     const [date, setDate] = useState("")
     const [loading, setLoading] = useState(true);
-    
-    
-    useEffect(() => 
-        fetch("http://localhost:8080/api/getArticle", 
-            {   method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"url": link})
-            }).then((response) => {return response.json()}).then((r) => {
-                 setTitle(r.title);  
-                 setText(r.text);  
-                 setAuthors(r.authors);  
-                 setDate(r.date); 
-                 setLoading(false); 
-                 console.log("new request was made")}), [loading])
-    
+
+
+    useEffect(() =>
+        fetch("http://localhost:8080/api/getArticle",
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "url": link })
+            }).then((response) => { return response.json() }).then((r) => {
+                setTitle(r.title);
+                setText(r.text);
+                setAuthors(r.authors);
+                setDate(r.date);
+                setLoading(false);
+                console.log("new request was made")
+            }), [loading])
+
     var articles
 
     if (loading) {
-        articles = <p>loading</p>
+        articles =
+        <div>
+            <Loader active inline='centered' className='settings' size='massive' style={{marginTop: '40%'}}/>
+        </div>
+            
     } else {
         articles = <Container>
-                        <Grid>
-                            <Link to="/">Back</Link>
-                        </Grid>
-                        <Container text>
-                            <Header as='h1'>{title}</Header>
-                            <Header as='h2'>{
-                                authors
-                            }</Header>
-                            <Header as='h3'>{moment(date).fromNow()}</Header>
-                            <p>
-                                {text}
-                            </p>
-                        </Container>
-                    </Container>
-        
+            <NavBar style={{ paddingTop: 40 }} />
+            <Grid>
+                <Link to="/">Back</Link>
+            </Grid>
+            <Container text>
+                <Header as='h1'>{title}</Header>
+                <Header as='h2'>{
+                    authors
+                }</Header>
+                <Header as='h3'>{moment(date).fromNow()}</Header>
+                <p>
+                    {text}
+                </p>
+            </Container>
+        </Container>
+
     }
 
-    
+
     return (
         <Container className="background">
             {articles}
         </Container>
-        
+
     )
 }
 
