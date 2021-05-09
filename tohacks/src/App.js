7ed27bbe51ab0bc563b38ec32af0ec095cd84601
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2021-05-08 23:26:08 -0400

    added menu to HomeMain

--- tohacks/src/App.js
+++ tohacks/src/App.js
@@ -12,7 +12,7 @@ import { Container } from 'semantic-ui-react';
 function App() {
   return (
     <Container>
-      {localStorage.getItem("config") ? <NavBar /> : <></>}
+      {localStorage.getItem("config") ? <></> : <></>}
       <Router>
         <Route exact path='/' component={Home} />
         {/* <Route exact path='/sports' component={Sports}/>
--- tohacks/src/components/HomeMain.js
+++ tohacks/src/components/HomeMain.js
@@ -1,21 +1,57 @@
 import React, {useEffect, useState} from 'react'
 import { Container, Grid, GridColumn } from 'semantic-ui-react'
 import NewsCard from './NewsCard'
+import { Input, Menu } from 'semantic-ui-react'
 
 const stop = 1;
 
 export default function HomeMain() {
+    const [current, setCurrent] = useState('latest news');
+    const config = JSON.parse(localStorage.getItem("config"));
+    const handleClick = (e, { name }) => {
+        localStorage.setItem('current-tab', name);
+        console.log(name)
+        setCurrent(localStorage.getItem('current-tab'))
+    };
     var currentTab = useState(localStorage.getItem('current-tab'));
     const [data, setData] = useState([], 0)
     useEffect(() => 
         fetch("http://localhost:8080/api/getCategory", 
             {   method: 'POST',
                 headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"kw": currentTab, "loc":"CA"})
+                body: JSON.stringify({"kw": current, "loc":"CA"})
             }).then((response) => {return response.json()}).then((r) => {
-                setData(r, [stop]); console.log("new request was made")}), localStorage.getItem('current-tab'))
+                setData(r, [stop]); console.log("new request was made")}), [current])
     return (
         <Container className="background">
+            <Menu secondary>
+            <Menu.Item
+                name='Latest News'
+                active={current === 'latest news'}
+                onClick={handleClick}/>
+                {config && Object.entries(config).map((key, val) => {
+                    if (key[1]) {
+                        console.log(key)
+                        return (
+                            <Menu.Item
+                                name={key[0]}
+                                active={current === {key}}
+                                onClick={handleClick}
+                            />
+                        )
+                    }
+                })}
+                <Menu.Menu position='right'>
+                    <Menu.Item>
+                        <Input icon='search' placeholder='Search...' />
+                    </Menu.Item>
+                    <Menu.Item
+                        name='logout'
+                        active={current === 'logout'}
+                        onClick={handleClick}
+                    />
+                </Menu.Menu>
+            </Menu>
             <Grid columns={3}>
                 <Grid.Row>
                     {
