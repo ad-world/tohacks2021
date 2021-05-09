77a7fd7f88f9487a6eda7a4877c89d3af399635e
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-09 10:07:16 +0400

    Revert "article pages work"
    This reverts commit 34e0a610222fb304f5bc8680f3c08904e02c2c31.

--- tohacks/src/components/HomeMain.js
+++ tohacks/src/components/HomeMain.js
@@ -40,7 +40,12 @@ export default function HomeMain() {
     const config = JSON.parse(localStorage.getItem("config"));
     const handleClick = (e, { name }) => {
         localStorage.setItem('current-tab', name);
+
+        console.log(name);
+
+        console.log(name)
         setLoading(true)
+
         setCurrent(localStorage.getItem('current-tab'))
     };
     var currentTab = useState(localStorage.getItem('current-tab'));
--- tohacks/src/pages/Article.js
+++ tohacks/src/pages/Article.js
@@ -4,61 +4,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Grid, Header } from 'semantic-ui-react';
 import moment from 'moment';
-import { useEffect, useState } from 'react'
 
 function SingleArticle(props) {
 
     const link = localStorage.getItem('link');
-    //var {title, text, authors, date} = {}
-    const [title, setTitle] = useState("")
-    const [text, setText] = useState("")
-    const [authors, setAuthors] = useState("")
-    const [date, setDate] = useState("")
-    const [loading, setLoading] = useState(true);
+    const {title, text, authors, date} = require('../article_sample.json');
+    /*
+
+    Create Query Here    
+    {return title, text, date}
+
+    */
+
+
     
 
-    useEffect(() => 
-        fetch("http://localhost:8080/api/getArticle", 
-            {   method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"url": link})
-            }).then((response) => {return response.json()}).then((r) => {
-                 setTitle(r.title);  
-                 setText(r.text);  
-                 setAuthors(r.authors);  
-                 setDate(r.date); 
-                 setLoading(false); 
-                 console.log("new request was made")}), [loading])
-    
-    var articles
-
-    if (loading) {
-        articles = <p>loading</p>
-    } else {
-        articles = <Container>
-                        <Grid>
-                            <Link to="/">Back</Link>
-                        </Grid>
-                        <Container text>
-                            <Header as='h1'>{title}</Header>
-                            <Header as='h2'>{
-                                authors
-                            }</Header>
-                            <Header as='h3'>{moment(date).fromNow()}</Header>
-                            <p>
-                                {text}
-                            </p>
-                        </Container>
-                    </Container>
-        
-    }
 
     
     return (
-        <Container className="background">
-            {articles}
+        <Container>
+            <Grid>
+                <Link to="/">Back</Link>
+            </Grid>
+            <Container text>
+                <Header as='h1'>{title}</Header>
+                <Header as='h2'>{
+                    authors
+                }</Header>
+                <Header as='h3'>{moment(date).fromNow()}</Header>
+                <p>
+                    {text}
+                </p>
+            </Container>
         </Container>
-        
     )
 }
 
